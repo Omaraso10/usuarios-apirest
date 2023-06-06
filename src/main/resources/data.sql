c604cf43754a859df149695106d09c5215441091
Omar enrique Alvarez garcia <omar.enrique.alvarez.garcia@gmail.com>
2023-06-06 12:16:56 -0400

    Commit inicial del proyecto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email, uuid, password, enabled, created, modified, last_login) VALUES ('Omar Alvarez', 'omar.alvarez@correo.cl', '38400000-8cf0-11bd-b23e-10b96e4ef00d', '$2a$10$T59OPi0/EJc47d.I86Ij9.BBtKXZMjJoH3Xk/GInFa3WHoBoLN6T2', 1, '2023-06-05', '2023-06-05', '2023-06-05');
+INSERT INTO phones (number, city_code, country_code, user_id) VALUES (58561480, '9', '56', 1);
+INSERT INTO phones (number, city_code, country_code, user_id) VALUES (45445557, '2', '56', 1);
+
+INSERT INTO users (name, email, uuid, password, enabled, created, modified, last_login) VALUES ('Juan Perez', 'juan.perez@correo.cl', 'f8c3de3d-1fea-4d7c-a8b0-29f63c4c3454', '$2a$10$R768vqGudSfQ9.AzaC4DX.5mtRxEI1KPt1ZDvvfy/3DJ6jz2zAxKi', 1, '2023-06-05', '2023-06-05', '2023-06-05');
+INSERT INTO phones (number, city_code, country_code, user_id) VALUES (49504832, '2', '56', 2);
---
+++ src/test/java/com/oalvarez/backend/usuario/apirest/UsuariosApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oalvarez.backend.usuario.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
